f6bd6fc466e1acf442ff527419f5a72dfae73096
cohouseol <cohouseol@gmail.com>
2026-03-27 12:23:35 +0900

    수정

--- packages/shared/src/api.ts
+++ packages/shared/src/api.ts
@@ -26,8 +26,8 @@ const createEndpoints = (baseUrl: string) => ({
   notiPublic: `${baseUrl}/notice/${version}/public`,
   faqPublic: `${baseUrl}/faq/${version}/public`,
   authPublic: `${baseUrl}/auth/${version}/public`,
-});
- 
+}); 
+
 export const getClientApiBaseUrl = () =>
     normalizeBaseUrl(process.env.NEXT_PUBLIC_API_URL);
 
