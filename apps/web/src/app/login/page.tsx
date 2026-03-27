f36f2c20831f5f195865ead99f064116716b58b7
cohouseol <cohouseol@gmail.com>
2026-03-27 14:01:20 +0900

    [prod] 태그 수정 및 네이버 로그인 현재 제거

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -118,29 +118,39 @@ export default function LoginPage() {
                         className="flex h-[54px] w-full items-center justify-center rounded-full border border-[#d8d8d8] bg-white px-5 text-[15px] font-semibold text-[#111] shadow-sm transition hover:bg-[#f8f8f8]"
                     >
                         <div className="flex w-full items-center">
-                            <div className="flex w-10 justify-start text-[26px] font-bold text-[#4285F4]">
-                                G
+
+                            {/* Google SVG Logo */}
+                            <div className="flex w-10 justify-start">
+                                <svg width="20" height="20" viewBox="0 0 48 48">
+                                    <path fill="#EA4335" d="M24 9.5c3.54 0 6.73 1.22 9.23 3.6l6.88-6.88C35.8 2.7 30.3 0 24 0 14.82 0 6.74 5.48 2.69 13.44l8.01 6.22C12.77 13.02 17.93 9.5 24 9.5z"/>
+                                    <path fill="#4285F4" d="M46.5 24.5c0-1.6-.14-3.13-.4-4.6H24v9h12.7c-.55 2.9-2.2 5.36-4.7 7.02l7.3 5.67C43.98 37.02 46.5 31.3 46.5 24.5z"/>
+                                    <path fill="#FBBC05" d="M10.7 28.66A14.5 14.5 0 0 1 9.5 24c0-1.63.28-3.2.78-4.66l-8.01-6.22A24 24 0 0 0 0 24c0 3.88.93 7.54 2.69 10.78l8.01-6.12z"/>
+                                    <path fill="#34A853" d="M24 48c6.3 0 11.6-2.08 15.47-5.64l-7.3-5.67c-2.02 1.36-4.6 2.16-8.17 2.16-6.07 0-11.23-3.52-13.3-8.56l-8.01 6.12C6.74 42.52 14.82 48 24 48z"/>
+                                </svg>
                             </div>
+
+                            {/* Text */}
                             <div className="flex-1 text-center pr-10">
                                 Google로 시작하기
                             </div>
+
                         </div>
                     </button>
 
-                    <button
-                        type="button"
-                        onClick={handleNaverLogin}
-                        className="mt-3 flex h-[54px] w-full items-center justify-center rounded-full bg-[#03c75a] px-5 text-[15px] font-semibold text-white transition hover:bg-[#02b350]"
-                    >
-                        <div className="flex w-full items-center">
-                            <div className="flex w-10 justify-start text-[22px] font-black">
-                                N
-                            </div>
-                            <div className="flex-1 text-center pr-10">
-                                Naver로 시작하기
-                            </div>
-                        </div>
-                    </button>
+                    {/*<button*/}
+                    {/*    type="button"*/}
+                    {/*    onClick={handleNaverLogin}*/}
+                    {/*    className="mt-3 flex h-[54px] w-full items-center justify-center rounded-full bg-[#03c75a] px-5 text-[15px] font-semibold text-white transition hover:bg-[#02b350]"*/}
+                    {/*>*/}
+                    {/*    <div className="flex w-full items-center">*/}
+                    {/*        <div className="flex w-10 justify-start text-[22px] font-black">*/}
+                    {/*            N*/}
+                    {/*        </div>*/}
+                    {/*        <div className="flex-1 text-center pr-10">*/}
+                    {/*            Naver로 시작하기*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</button>*/}
                 </div>
 
                 <div className="mt-6 flex justify-center gap-3 text-[13px] text-[#8c8c9a]">
