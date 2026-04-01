9d3b05aaddef70b30ce1c184282978d0de41b577
cohouseol <cohouseol@gmail.com>
2026-04-01 09:15:26 +0900

    수정

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://mockio-cloud/sitemap.xml',
+        sitemap: 'https://mockio.cloud/sitemap.xml',
     }
 }
