512a0f0a9c14241d03cbf5a268be197d7ea03962
cohouseol <cohouseol@gmail.com>
2026-04-04 18:49:36 +0900

    open api 추가

---
+++ apps/web/src/app/docs/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { ApiReferenceReact } from "@scalar/api-reference-react"
+import "@scalar/api-reference-react/style.css"
+import "./scalar-notion.css"
+
+export default function ApiDocsPage() {
+    return (
+        <div className="docs-shell">
+            <ApiReferenceReact
+                configuration={{
+                    sources: [
+                        {
+                            title: "Auth Service",
+                            url: "http://localhost:9000/docs/auth/v3/api-docs",
+                        },
+                        {
+                            title: "Core Service",
+                            url: "http://localhost:9000/docs/core/v3/api-docs",
+                        },
+                        {
+                            title: "Support Service",
+                            url: "http://localhost:9000/docs/support/v3/api-docs",
+                        },
+                    ],
+                    theme: "default",
+                    hiddenClients: true,
+                    hideDownloadButton: true,
+                    hideTestRequestButton: true,
+                }}
+            />
+        </div>
+    )
+}
